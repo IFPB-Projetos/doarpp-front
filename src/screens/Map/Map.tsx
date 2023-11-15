dcaaf048ac7955a6d7eb4b0115076c03e428ffc7
Robson William <robsonwilliamro@gmail.com>
2023-11-14 22:24:34 -0300

    Adicionando leaflet(mapa) ao projeto

--- src/screens/Map/Map.tsx
+++ src/screens/Map/Map.tsx
@@ -1,9 +1,25 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 import "./styles.css";
 
 export default function Map(){
+    const initialPosition = [-22.2154042, -54.8331331]
+
     return (
         <>
-            <h1>Mapa</h1>
+            <div id="map-body">
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </>
     )
 }
