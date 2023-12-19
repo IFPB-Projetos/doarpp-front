072b795e65549738b2a4bb2120834e876d1423e6
Diuari <diuaripessoa07@gmail.com>
2023-12-19 06:28:28 -0300

    validacao

--- src/components/InputComment/InputComment.tsx
+++ src/components/InputComment/InputComment.tsx
@@ -4,39 +4,56 @@ import "./style.css";
 import { useAuth } from "../../contexts/auth";
 
 type Props = {
-    postId: string
-}
+  postId: string;
+};
 
-export default function InputComment({postId}:Props){
-    const [content, setContent] = useState("");
+export default function InputComment({ postId }: Props) {
+  const [content, setContent] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
-    const {signed} = useAuth();
+  const { signed } = useAuth();
 
-    async function handleCreateComment(event: FormEvent<HTMLFormElement>){
-        event.preventDefault();
-        try {
-            await api.post("/comments/", {content, postId})
-            window.location.reload()
-          } catch (err) {
-            console.log(err)
-          }
+  async function handleCreateComment(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!content.trim()) {
+      setError("O comentário não pode ser vazio");
+      return;
     }
 
-    return (
-        <form id="input-comment-body" onSubmit={handleCreateComment} method="POST">
-            {signed ?
-            <>
-                <input type="hidden" value={postId} name="postId" />
-                <textarea
-                    placeholder="Comentário"
-                    name="content"
-                    id="input-comment-content"
-                    value={content}
-                    onChange={(event) => setContent(event.target.value)}/>
-                
-                <button type="submit" id="input-comment-button">Enviar</button>
-            </>
-            : null}
-        </form>
-    )
+    try {
+      await api.post("/comments/", { content, postId });
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <form
+      id="input-comment-body"
+      onSubmit={handleCreateComment}
+      method="POST"
+    >
+      {signed ? (
+        <>
+          <input type="hidden" value={postId} name="postId" />
+          <textarea
+            placeholder="Comentário"
+            name="content"
+            id="input-comment-content"
+            value={content}
+            onChange={(event) => {
+              setContent(event.target.value);
+              setError(null);
+            }}
+          />
+          {error && <span className="comment-error">{error}</span>}
+          <button type="submit" id="input-comment-button">
+            Enviar
+          </button>
+        </>
+      ) : null}
+    </form>
+  );
 }
